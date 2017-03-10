7afb7f93ef4cac86cc9a5a1b0aedc2feec6b8580
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-10 13:15:19 +0200

    Removed duplicate functions

--- src/app/http/controllers/HCURLShortenerController.php
+++ src/app/http/controllers/HCURLShortenerController.php
@@ -160,37 +160,7 @@ class HCURLShortenerController extends HCBaseController
         return $list;
     }
 
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function pageData()
-    {
-        return $this->createQuery()->paginate($this->recordsPerPage);
-    }
 
-    /**
-     * Creating data list based on search
-     * @return mixed
-     */
-    public function search()
-    {
-        if (!request('q'))
-            return [];
-
-        //TODO set limit to start search
-
-        return $this->list();
-    }
-
-    /**
-     * Creating data list
-     * @return mixed
-     */
-    public function list()
-    {
-        return $this->createQuery()->get();
-    }
 
     /**
      * List search elements
