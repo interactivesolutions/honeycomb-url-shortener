68685d3111c5ca8cef68a2eee8d55f57117bce3a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-16 10:14:27 +0200

    Updated service provider logic

--- src/app/providers/HCURLShortenerServiceProvider.php
+++ src/app/providers/HCURLShortenerServiceProvider.php
@@ -42,11 +42,11 @@ class HCURLShortenerServiceProvider extends ServiceProvider
     /**
      * Register helper function
      */
-    private function registerHelpers()
+    private function registerHelpers ()
     {
-        $filePath = __DIR__ . '/../Http/helpers.php';
+        $filePath = __DIR__ . '/../http/helpers.php';
 
-        if (\File::isFile($filePath))
+        if (\File::isFile ($filePath))
             require_once $filePath;
     }
 
@@ -55,25 +55,34 @@ class HCURLShortenerServiceProvider extends ServiceProvider
      */
     private function registerPublishElements ()
     {
+        $directory = __DIR__ . '/../../database/migrations/';
+
         // Publish your migrations
-        $this->publishes([
-            __DIR__ . '/../../database/migrations/' => database_path('/migrations'),
-        ], 'migrations');
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
+            ], 'migrations');
+
+        $directory = __DIR__ . '/../public';
 
         // Publishing assets
-        $this->publishes([
-            __DIR__ . '/../public' => public_path('honeycomb'),
-        ], 'public');
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../public' => public_path ('honeycomb'),
+            ], 'public');
     }
 
     /**
      * Registering routes
      */
-    private function registerRoutes()
+    private function registerRoutes ()
     {
-        \Route::group(['namespace' => $this->namespace], function ($router) {
-            require __DIR__ . '/../../app/honeycomb/routes.php';
-        });
+        $filePath = __DIR__ . '/../../app/honeycomb/routes.php';
+
+        if (file_exists($filePath))
+            \Route::group (['namespace' => $this->namespace], function ($router) use ($filePath) {
+                require $filePath;
+            });
     }
 }
 
