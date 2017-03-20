05f0103e1f821e1ddf0b114c7c10f28f60d6641a
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-20 14:16:42 +0200

    Updated service provider

--- src/app/providers/HCURLShortenerServiceProvider.php
+++ src/app/providers/HCURLShortenerServiceProvider.php
@@ -2,15 +2,18 @@
 
 namespace interactivesolutions\honeycomburlshortener\app\providers;
 
-use Illuminate\Support\ServiceProvider;
+use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
 
-class HCURLShortenerServiceProvider extends ServiceProvider
+class HCURLShortenerServiceProvider extends HCBaseServiceProvider
 {
     protected $homeDirectory = __DIR__;
 
     protected $commands = [];
 
     protected $namespace = 'interactivesolutions\honeycomburlshortener\app\http\controllers';
+
+    public $serviceProviderNameSpace = 'HCURLShortener';
+
 }
 
 
